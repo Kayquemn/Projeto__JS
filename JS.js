b2e33fbe75915dfeb6b63c3beab7a75429cce837
Kayquemn <145171658+Kayquemn@users.noreply.github.com>
2024-02-29 21:08:36 -0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -5,9 +5,45 @@ titulo.textcontent="Lista de encomendas";
 
 var linhastabela = document.querySelectorAll(".item");
 
+
+
+
 for (i = 0 ; i < linhastabela.length; i++){
-   
-    linhastabela[i].querySelector(".preco-total").textContent=linhastabela[i].querySelector(".preco-unitario").textContent * linhastabela[i].querySelector(".quantidade").textContent;
+   var qtde = linhastabela[i].querySelector(".quantidade");
+    var valor = linhastabela[i].querySelector(".preco-unitario");
+
+    if(qtde.textContent<1 || isNaN(qtde.textContent)){
+        //Quantidade NOK, avisa o usuário
+        // clientes[count].querySelector(".quantidade").textContent = "QTDE INVALIDA!"; 
+        qtde.textContent="QTDE INVALIDO";
+        qtde.style.color="red"
+        //Clientes[count].style.color="red";
+        
+    }else{
+        linhastabela[i].querySelector(".preco-total").textContent=calculaTotal(valor.textContent,qtde.textContent);
+    }
+
+    // linhastabela[i].querySelector(".preco-total").textContent=linhastabela[i].querySelector(".preco-unitario").textContent * linhastabela[i].querySelector(".quantidade").textContent;
+    if(valor.textContent<1 || isNaN(valor.textContent)){
+        //Quantidade NOK, avisa o usuário
+        // clientes[count].querySelector(".quantidade").textContent = "QTDE INVALIDA!"; 
+        valor.textContent="VALOR INVALIDO";
+        linhastabela[i].style.backgroundColor = "red"
+        //Clientes[count].style.color="red";
+        
+    }
+else{
+    linhastabela[i].querySelector(".preco-total").textContent=calculaTotal(valor.textContent,qtde.textContent);
+}
+}
 
 
+
+
+//Criando função para calcular o valor total da encomenda 
+function calculaTotal(qtde,unitario){
+    var total = 0;
+    total=qtde*unitario;
+
+    return total;
 }
